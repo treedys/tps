25f95c470a1fecb44cd2e3d82cded3203aa5e470
Atanas Palavrov <palavrov@gmail.com>
2017-11-20 00:13:45 +0200

    Add ESPRESSObin controller support

---
+++ controller/external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_TREEDYS_CONTROLLER_PATH)/package/*/*.mk))
--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 make -C buildroot/ O=../output/camera BR2_EXTERNAL=../camera/ scanner_camera_defconfig
+make -C buildroot/ O=../output/controller BR2_EXTERNAL=../controller/ scanner_controller_defconfig
 make -C output/camera/ menuconfig
+make -C output/controller/ menuconfig
