1e80b722bf107202b367fe446c651e99478032e6
Atanas Palavrov <palavrov@gmail.com>
2018-01-08 23:00:37 +0200

    Empty camera-app C/C++ project

--- camera/external.mk
+++ camera/external.mk
@@ -1,1 +1,2 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_TREEDYS_CAMERA_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_TREEDYS_CAMERA_PATH)/package/treedys/*/*.mk))
---
+++ camera/package/treedys/camera-app/camera-app.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# Treedys camera app
+#
+################################################################################
+
+CAMERA_APP_SITE = $(BR2_EXTERNAL_TREEDYS_CAMERA_PATH)/app
+CAMERA_APP_SITE_METHOD = local
+
+CAMERA_APP_INSTALL_TARGET = YES
+CAMERA_APP_INSTALL_STAGING = NO
+CAMERA_APP_DEPENDENCIES = rpi-userland
+
+define CAMERA_APP_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+endef
+
+define CAMERA_APP_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 -t $(TARGET_DIR)/bin/ $(@D)/camera-app
+endef
+
+$(eval $(generic-package))
