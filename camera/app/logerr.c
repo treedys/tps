7a4a63f1f1a414fe284d5423ed255b1a2bccc48a
Atanas Palavrov <palavrov@gmail.com>
2018-03-11 20:42:30 +0200

    Refactoring logerr

--- camera/app/logerr.c
+++ camera/app/logerr.c
@@ -2,7 +2,6 @@
 #include "logerr.h"
 
 #include <pthread.h>
-#include <errno.h>
 
 #include <stdio.h>
 #include <stdint.h>
@@ -22,35 +21,65 @@ const void * const log_pc(void)
     return __builtin_return_address(0);
 }
 
-void log_error(const void * const addr, const char * const format, ... )
+static void log_message(const char * const message)
+{
+    (void)fputs(message, stderr);
+    (void)fputs("\n", stderr);
+}
+
+void log_error(int caller_errno, const void * const addr, const char * const format, ... )
 {
     /* Get the time as soon as possible */
     time_t t;
     errno = 0;
     t = time(NULL);
-    int t_errno = (t==-1) ? errno : 0;
+    int result;
+
+    char message[500];
 
 #ifdef LOGERR_THREAD_SAFE
     /* Try to lock. In case of error report it and continue,
      * we cannot do something better. */
     int lock_err = pthread_mutex_lock(&logerr_mutex);
     if(lock_err!=0)
-        (void)fprintf(stderr, "L%07X\n", lock_err);
+    {
+        result = snprintf(&message[0], sizeof(message), "L%08X", lock_err);
+
+        if(result>0)
+            log_message(message);
+    }
 #endif
 
-    /* Print time of error and address */
-    if(t_errno==0)
-        (void)fprintf(stderr, "%08X %08"PRIXPTR" ", (uint32_t)t, (uintptr_t)addr);
-    else
-        (void)fprintf(stderr, "T%07X %08"PRIXPTR" ", (uint32_t)t_errno, (uintptr_t)addr);
+    size_t size = 0;
+
+    /* Print error time and address */
+    result = snprintf(&message[size], sizeof(message)-size, "%08X %08"PRIXPTR" ", (uint32_t)t, (uintptr_t)addr);
+
+    if(result>0)
+        size += result;
+
+    /* Print human readable errno */
+    if(caller_errno)
+    {
+        errno = 0;
+        const char * strerr = strerror(caller_errno);
+
+        result = snprintf(&message[size], sizeof(message)-size, ": \"%s\" errno(%d)", errno==0 ? strerr : "Unknown", caller_errno);
+
+        if(result>0)
+            size += result;
+    }
 
     /* Print actual error */
     va_list ap;
     va_start(ap, format);
-    (void)vfprintf(stderr, format, ap);
+    result = vsnprintf(&message[size], sizeof(message)-size, format, ap);
     va_end(ap);
 
-    (void)printf("\n");
+    if(result>0)
+        size += result;
+
+    log_message(message);
 
 #ifdef LOGERR_THREAD_SAFE
     /* Unlock if lock was successful */
@@ -59,58 +88,12 @@ void log_error(const void * const addr, const char * const format, ... )
         int unlock_err = pthread_mutex_unlock(&logerr_mutex);
 
         if(unlock_err!=0)
-            (void)fprintf(stderr, "U%07X\n", unlock_err);
-    }
-#endif
-}
+        {
+            result = snprintf(&message[0], sizeof(message), "U%08X\n", unlock_err);
 
-void log_errno(const void * const addr, const char * const format, ... )
-{
-    int caller_errno = errno;
-
-    /* Get the time as soon as possible */
-    time_t t;
-    errno = 0;
-    t = time(NULL);
-    int t_errno = (t==-1) ? errno : 0;
-
-#ifdef LOGERR_THREAD_SAFE
-    /* Try to lock. In case of error report it and continue,
-     * we cannot do something better. */
-    int lock_err = pthread_mutex_lock(&logerr_mutex);
-    if(lock_err!=0)
-        (void)fprintf(stderr, "L%07X\n", lock_err);
-#endif
-
-    /* Print time or error and address */
-    if(t_errno==0)
-        (void)fprintf(stderr, "%08X %08"PRIXPTR" ", (uint32_t)t, (uintptr_t)addr);
-    else
-        (void)fprintf(stderr, "T%07X %08"PRIXPTR" ", (uint32_t)t_errno, (uintptr_t)addr);
-
-    /* Print user error message */
-    va_list ap;
-    va_start(ap, format);
-    (void)vfprintf(stderr, format, ap);
-    va_end(ap);
-
-    /* Print human readable errno */
-    errno = 0;
-    const char * strerr = strerror(caller_errno);
-
-    if(errno==0)
-        (void)fprintf(stderr, ": %s\n", strerr);
-    else
-        (void)fprintf(stderr, ": Unknown error (%d)\n", caller_errno);
-
-#ifdef LOGERR_THREAD_SAFE
-    /* Unlock if lock was successful */
-    if(lock_err==0)
-    {
-        int unlock_err = pthread_mutex_unlock(&logerr_mutex);
-
-        if(unlock_err!=0)
-            (void)fprintf(stderr, "U%07X\n", unlock_err);
+            if(result>0)
+                log_message(message);
+        }
     }
 #endif
 }
--- camera/app/logerr.h
+++ camera/app/logerr.h
@@ -20,18 +20,16 @@
 #define  LOGERR_LOGERR_INC
 
 #include <stdarg.h>
+#include <errno.h>
 
-#define LOG_ERROR(format, ... ) do { log_error(log_pc(), format, ##__VA_ARGS__); } while(0)
-#define LOG_ERRNO(format, ... ) do { log_errno(log_pc(), format, ##__VA_ARGS__); } while(0)
+#define LOG_ERROR(format, ... ) do { log_error(    0, log_pc(), format, ##__VA_ARGS__); } while(0)
+#define LOG_ERRNO(format, ... ) do { log_error(errno, log_pc(), format, ##__VA_ARGS__); } while(0)
 
 #define LOG_MESSAGE(fmt, ...) // LOG_ERROR(fmt, ##__VA_ARGS__)
 
 const void * const log_pc(void);
 
-__attribute__((format(printf,2,3)))
-void log_error(const void * const addr, const char * const format, ... );
-
-__attribute__((format(printf,2,3)))
-void log_errno(const void * const addr, const char * const format, ... );
+__attribute__((format(printf,3,4)))
+void log_error(int caller_errno, const void * const addr, const char * const format, ... );
 
 #endif
