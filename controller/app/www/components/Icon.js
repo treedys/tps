0ee5c7e4cb3a00f398881fe0cf46acced8b8eb30
Atanas Palavrov <atanas@treedys.com>
2019-09-09 13:41:42 +0200

    Fixup react-svg v10 svgStyle removal

--- controller/app/www/components/Icon.js
+++ controller/app/www/components/Icon.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactSVG from 'react-svg';
+import reactToCSS from 'react-style-object-to-css';
 
 export default ({ style, ...props}) =>
     props.src.toLowerCase().endsWith("svg")
-        ? <ReactSVG svgStyle={style} {...props}/>
-        : <div><img    style={style} {...props}/></div>
+        ? <ReactSVG beforeInjection={ svg => svg.setAttribute('style', reactToCSS(style)) } {...props}/>
+        : <div><img style={style} {...props}/></div>
