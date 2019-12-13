f3f1fe26917069a8c1e1a65db252750f631493f7
Atanas Palavrov <palavrov@gmail.com>
2019-12-13 12:49:07 +0100

    MoreProps with react hooks

--- controller/app/www/components/MoreProps.js
+++ controller/app/www/components/MoreProps.js
@@ -1,30 +1,23 @@
 import React from 'react';
 
-export default class MoreProps extends React.Component {
+export default props => {
 
-    state = { focused: false, hovered: false, pressed: false };
+    const [ focused, setFocused ] = React.useState(false);
+    const [ hovered, setHovered ] = React.useState(false);
+    const [ pressed, setPressed ] = React.useState(false);
 
-    handleInputFocus = () => this.setState({ focused: true  });
-    handleInputBlur  = () => this.setState({ focused: false });
-    handleMouseEnter = () => this.setState({ hovered: true  });
-    handleMouseLeave = () => this.setState({ hovered: false });
-    handleMouseDown  = () => this.setState({ pressed: true  });
-    handleMouseUp    = () => this.setState({ pressed: false });
+    const child = React.Children.only(props.children);
 
-    render() {
-        const child = React.Children.only(this.props.children);
-
-        return <child.type {...child.props}
-                    focused = { this.state.focused }
-                    hovered = { this.state.hovered }
-                    pressed = { this.state.pressed }
-                    onFocus      = { event => { this.handleInputFocus(); child.onFocus     ?.(event); } }
-                    onBlur       = { event => { this.handleInputBlur (); child.onBlur      ?.(event); } }
-                    onMouseEnter = { event => { this.handleMouseEnter(); child.onMouseEnter?.(event); } }
-                    onMouseLeave = { event => { this.handleMouseLeave(); child.onMouseLeave?.(event); } }
-                    onMouseDown  = { event => { this.handleMouseDown (); child.onMouseDown ?.(event); } }
-                    onMouseUp    = { event => { this.handleMouseUp   (); child.onMouseUp   ?.(event); } }>
-                    {child.props.children}
-                </child.type>
-    }
+    return <child.type {...child.props}
+        focused = { focused }
+        hovered = { hovered }
+        pressed = { pressed }
+        onFocus      = { event => { setFocused(true ); child.onFocus     ?.(event); } }
+        onBlur       = { event => { setFocused(false); child.onBlur      ?.(event); } }
+        onMouseEnter = { event => { setHovered(true ); child.onMouseEnter?.(event); } }
+        onMouseLeave = { event => { setHovered(false); child.onMouseLeave?.(event); } }
+        onMouseDown  = { event => { setPressed(true ); child.onMouseDown ?.(event); } }
+        onMouseUp    = { event => { setPressed(false); child.onMouseUp   ?.(event); } }>
+        {child.props.children}
+    </child.type>
 };
